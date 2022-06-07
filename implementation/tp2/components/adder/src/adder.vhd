8a4413e1aeeb6fdb8520572db623d9ce7e1f829b
lucasjsch <lucas.scheinkerman@gmail.com>
2022-06-08 00:02:39 +0300

    More changes to TP2 draft.

---
+++ implementation/tp2/components/adder/src/adder.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+-- TODO: Test this
+entity adder is
+    generic(N: integer:= 4);
+    port(
+        X0        : in std_logic_vector(N-1 downto 0);
+        X1        : in std_logic_vector(N-1 downto 0);
+        carry_in  : in std_logic;
+        Y         : out std_logic_vector(N-1 downto 0);
+        carry_out : out std_logic
+    );
+end;
+
+architecture adder_arch of adder is
+     signal out_aux: std_logic_vector(N+1 downto 0);
+begin
+    out_aux <= std_logic_vector(unsigned('0' & X0 & carry_in) + unsigned('0' & X1 & '1'));
+    Y <= out_aux(N downto 1);				
+    carry_out <= out_aux(N+1);				
+end;
--- implementation/tp2/components/fp_sum/src/fp_sum.vhd
+++ implementation/tp2/components/fp_sum/src/fp_sum.vhd
@@ -58,22 +58,33 @@ architecture fp_sum_arch of fp_sum is
 	signal result_exponent       : unsigned(EXPONENT_BITS+1 downto 0) := (others => '0');
 	signal result_mantissa       : unsigned(MANTISSA_BITS-1 downto 0) := (others => '0');
 
-    signal xor_sign              : std_logic;
+    signal xor_sign              : std_logic := '0';
     -- Exponent registers need one more bit to be able to compute the unsigned operations.
     signal a_exp                 : signed(EXPONENT_BITS-1 downto 0) := (others => '0');
     signal b_exp                 : signed(EXPONENT_BITS-1 downto 0) := (others => '0');
     signal aux_exp               : signed(EXPONENT_BITS-1 downto 0) := (others => '0');
+    signal final_exp             : signed(EXPONENT_BITS-1 downto 0) := (others => '0');
 
     -- Significand registers need two more bit to be able to detect overflow and add the '1' bit on top.
-    signal a_mantissa            : unsigned(MANTISSA_BITS downto 0);
-    signal b_mantissa            : unsigned(MANTISSA_BITS downto 0);
-    signal b_shifted_mantissa    : unsigned(MANTISSA_BITS downto 0);
-    signal aux_mantissa          : unsigned(MANTISSA_BITS*2+1 downto 0);
-    signal preliminary_mantissa  : unsigned(MANTISSA_BITS downto 0);
+    signal a_mantissa            : unsigned(MANTISSA_BITS downto 0) := (others => '0');
+    signal b_mantissa            : unsigned(MANTISSA_BITS downto 0) := (others => '0');
+    signal b_shifted_mantissa    : unsigned(MANTISSA_BITS downto 0) := (others => '0');
+    signal aux_mantissa          : unsigned(MANTISSA_BITS*2+1 downto 0) := (others => '0');
+    signal preliminary_mantissa  : unsigned(MANTISSA_BITS downto 0) := (others => '0');
+    signal preliminary_mantissa_2: unsigned(MANTISSA_BITS downto 0) := (others => '0');
+    signal final_mantissa        : unsigned(MANTISSA_BITS downto 0) := (others => '0');
 
     signal are_swapped           : std_logic := '0';
-    signal exp_selection         : std_logic_vector(1 downto 0);
-    signal mantissa_selection    : std_logic_vector(1 downto 0);
+    signal exp_selection         : std_logic_vector(1 downto 0) := (others => '0');
+    signal mantissa_selection    : std_logic_vector(1 downto 0) := (others => '0');
+    signal carry_out             : std_logic := '0';
+
+    signal shift_exp_bits        : signed(MANTISSA_BITS downto 0);
+    signal carry_out             : std_logic;
+    signal flag_bits             : std_logic_vector(7 downto 0);
+    signal g_bit                 : std_logic := '0';
+    signal r_bit                 : std_logic := '0';
+    signal s_bit                 : std_logic := '0';
 
 
     function A2_COMPLEMENT(X : std_logic_vector(b_mantissa'length-2 downto 0))
@@ -150,17 +161,61 @@ begin
     end process;
 
     -- Step 3: Shifting the B register according to the exponent difference
-    b_shifted_mantissa <= shift_right(b_mantissa, to_integer(a_exp - b_exp)) when xor_sign = '0' else
-                          unsigned(shift_right(signed(b_mantissa), to_integer(a_exp - b_exp)));
+    process(a_exp, b_exp)
+    begin
+        shift_exp_bits <= to_integer(a_exp - b_exp);
+        if (shift_exp_bits < MANTISSA_BITS) then
+            b_shifted_mantissa <= shift_right(b_mantissa, shift_exp_bits) when xor_sign = '0' else
+                                  unsigned(shift_right(signed(b_mantissa), shift_exp_bits));
+            flag_bits <= rotate_right(b_mantissa, shift_exp_bits)(MANTISSA_BITS downto MANTISSA_BITS-7) when xor_sign = '0' else
+                         unsigned(rotate_right(signed(b_mantissa), shift_exp_bits))(MANTISSA_BITS downto MANTISSA_BITS-7);
+        else
+            b_shifted_mantissa <= (others => '0');
+            flag_bits <= rotate_right(b_mantissa, shift_exp_bits)(MANTISSA_BITS downto MANTISSA_BITS-7) when xor_sign = '0' else
+                         unsigned(rotate_right(signed(b_mantissa), shift_exp_bits))(MANTISSA_BITS downto MANTISSA_BITS-7);
+        end if;
+    end process;
+
+    process(flag_bits)
+    begin
+        g_bit <= flag_bits'left;
+        r_bit <= flag_bits(6);
+        s_bit <= '0' when flag_bits(5 downto 0) = "000000" else '1';
+    end process;
 
     -- Step 4: Compute preliminary mantissa
-    preliminary_mantissa <= b_shifted_mantissa + a_mantissa;
+    preliminary_mantissa_adder : mux
+    generic map(MANTISSA_BITS+1)
+    port map (
+        X0          => b_shifted_mantissa,
+        X1          => a_mantissa,	
+        carry_in    => '0',
+        unsigned(Y) => preliminary_mantissa,
+        carry_out   => carry_out);
+    process(preliminary_mantissa, carry_out, xor_sign)
+    begin
+        if (xor_sign = '1' and preliminary_mantissa'left = '1' and carry_out = '0') then
+            preliminary_mantissa_2 <= A2_COMPLEMENT(preliminary_mantissa);
+        else
+            preliminary_mantissa_2 <= preliminary_mantissa;
+        end if;
+    end process;
 
     -- Step 5: Compute final mantissa
+    final_mantissa <= ('1' & preliminary_mantissa_2) when (xor_sign = '0' and carry_out = '1') else
+                      normalize_mantissa();
+
+    final_exp  <= aux_exp when (xor_sign = '0' and carry_out = '1') else normalize_exp();
+    
+    -- Step 6: Adjust 'r' and 's' bits
     
 
-    -- Step 6: Adjust 'r' and 's' bits
     -- Step 7: Approximating the mantissa
+    
+
     -- Step 8: Computing the result's sign
+    result_sign <= b(SIGN_START_BIT) when (are_swapped = '1') else
+                   a(SIGN_START_BIT) when (are_swapped = '0' and xor_sign = '0') else
+                   b(SIGN_START_BIT) when (are_swapped = '0' and xor_sign = '1');
 
 end;
