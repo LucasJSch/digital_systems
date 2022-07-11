a38f077131abe73a3b149eb317bef034fc0d78da
lucasjsch <lucas.scheinkerman@gmail.com>
2022-07-11 23:37:10 +0300

    [TP3] Add automated test for cordic_processor.

--- tp3/testing/cordic_testdata_gen.py
+++ tp3/testing/cordic_testdata_gen.py
@@ -5,7 +5,7 @@ import numpy as np
 
 MAX_ANGLE = math.pi / 2.0
 MIN_ANGLE = - math.pi / 2.0
-N_SAMPLES = 100
+N_SAMPLES = 1000
 # 1 for sign, 1 for overflow.
 # Thus 2**(N_BITS_ANGLE-2) is equivalent to 45 degrees.
 N_BITS_ANGLE = 17
@@ -15,8 +15,8 @@ CORDIC_GAIN = 1.647
 cwd = os.getcwd()
 
 with open(cwd + '/testdata.txt', 'w', encoding='UTF8') as f:
-    writer = csv.writer(f)
-    writer.writerow(['x_i', 'y_i', 'beta', 'N_BITS_ANGLE', 'x_o', 'y_o'])
+    writer = csv.writer(f, delimiter=' ')
+    writer.writerow(['x_i', 'y_i', 'beta', 'x_o', 'y_o'])
 
     for i in np.linspace(start=MIN_ANGLE, stop=MAX_ANGLE, num=N_SAMPLES):
         x = int(CORDIC_GAIN * VECTOR_LENGTH * math.cos(i))
